985ef713cda098823b7758262498c0d5c69aa495
rodrixd123 <communityeren@gmail.com>
2025-09-24 11:33:27 -0500

    equipo 1 - avance

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.http import JsonResponse
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('core.urls')),
+    path('', lambda request: JsonResponse({"message": "Bienvenido a la API del Equipo 1 🚀"})),
 ]
+
